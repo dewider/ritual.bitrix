0848ce39874d790b3f42320d8e8d730af92b069a
Denis <denis.r.fl@yandex.ru>
2021-01-22 14:15:36 +0300

    исправлен main.feedback

--- bitrix/components/ivit/main.feedback/templates/.default/script.js
+++ bitrix/components/ivit/main.feedback/templates/.default/script.js
@@ -1,18 +1,9 @@
 BX.ready( function(){
 
     $('.mf-phone input').inputmask("+7(999)999-99-99");
-
-    var observer = new MutationObserver( function(mutations){
-        mutations.forEach(function(mutation){
-            if( mutation.previousSibling.nodeName == "IFRAME" );
-            $('.mf-phone input').inputmask("+7(999)999-99-99");
-        });
+    
+    BX.addCustomEvent('onAjaxSuccess', function(){
+ 
+        $('.mf-phone input').inputmask("+7(999)999-99-99");
     });
-
-    observer.observe(
-        document.body,
-        {
-            childList: true
-        }
-    )
 });
